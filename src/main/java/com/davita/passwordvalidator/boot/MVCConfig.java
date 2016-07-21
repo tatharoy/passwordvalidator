c7fa5f8ebdf414eaf16965a26c894c24c234779c
tatharoy <tatha.roy@gmail.com>
2016-07-21 15:46:42 -0600

    2nd checkin

--- src/main/java/com/davita/passwordvalidator/boot/MVCConfig.java
+++ src/main/java/com/davita/passwordvalidator/boot/MVCConfig.java
@@ -13,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 /**
  * MVC Configuration file ..
+ * this is test
  */
 @EnableWebMvc
 @Configuration("mvcConfig")
